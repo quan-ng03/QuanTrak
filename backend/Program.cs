e426508d941a68de9dca9dd61ef2811107107969
Quan Nguyen <jacob.nguyenaq@gmail.com>
2025-02-08 14:18:53 -0700

    SwaggerUI for API view

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,24 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo("/app/keys"))
-    .SetApplicationName("CountryTracker");
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Country Tracker API v1");
+});
 
 app.UseAuthorization();
 
