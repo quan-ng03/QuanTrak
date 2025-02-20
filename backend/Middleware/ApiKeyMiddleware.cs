74f6cd8298453d42c21716545468cb82d5394c1e
Quan Nguyen <jacob.nguyenaq@gmail.com>
2025-02-19 22:45:51 -0700

    Added API Auth.

---
+++ backend/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using backend.Services;
+
+namespace backend.Middleware;
+
+public class ApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private const string API_KEY_HEADER_NAME = "X-Api-Key";
+
+    public ApiKeyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, ApiValidator validator)
+    {
+        // First check if the header exist
+        if (!context.Request.Headers.TryGetValue(API_KEY_HEADER_NAME, out var value))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key was not provided.");
+            return;
+        }
+
+        // Then use the API Validator service to check if the key is correct
+        if (!validator.Validate(value))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key mismatch.");
+            return;
+        }
+
+        await _next(context);
+
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,18 +1,49 @@
 using backend.Data;
+using backend.Middleware;
 using backend.Services;
 using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Enter your API key:",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "X-Api-Key",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
 
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddDbContext<CountryContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddScoped<CountryService>();
+
+// API Key
+string? apiKey = builder.Configuration["API_KEY"];
+builder.Services.AddSingleton(new ApiValidator(apiKey));
+
 
 var app = builder.Build();
 
@@ -22,9 +53,12 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ApiKeyMiddleware>();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
 app.CreateDbIfNotExists();
 
 app.Run();
